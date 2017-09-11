31e75917fe3eddbd78dbb396b7dc10e8bd933f89
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-09-11 04:08:58 -0300

    Avoid creating AUTHORS and ChangeLog files

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 import setuptools
+
+
+os.environ['SKIP_GENERATE_AUTHORS'] = '1'
+os.environ['SKIP_WRITE_GIT_CHANGELOG'] = '1'
 
 
 def setup_package():
