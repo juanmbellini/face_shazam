0ac9ec564ab784c7086333758e7b88781eb7d80d
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-09-11 01:31:10 -0300

    Fix setup
    - Remove pyscaffold from setup_requires
    - Remove install_requires
    - Remove name and version from setup() function (is taken from setup.cfg)
    - Remove unnecessary stuff from setup.cfg
    - Fix __init__.py to log in case of failure (will log a warning if version is unknown)
    - Add a main.py file, which is the entry point of the application

---
+++ face_shazam/main.py
@@ -0,0 +1,74 @@
+import argparse
+import logging
+import sys
+
+from face_shazam import __version__
+
+_logger = logging.getLogger(__name__)
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args ([str]): command line parameters as list of strings
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(
+        description="Face Shazam")
+    parser.add_argument(
+        '-V',
+        '--version',
+        action='version',
+        version='face_shazam {ver}'.format(ver=__version__))
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action='store_const',
+        const=logging.INFO)
+    parser.add_argument(
+        '-vv',
+        '--very-verbose',
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action='store_const',
+        const=logging.DEBUG)
+    return parser.parse_args(args)
+
+
+def setup_logging(log_level):
+    """Setup basic logging
+
+    Args:
+      log_level (int): minimum loglevel for emitting messages
+    """
+    logging.basicConfig(level=log_level,
+                        stream=sys.stdout,
+                        format="[%(asctime)s] %(levelname)s:%(name)s: %(message)s",
+                        datefmt="%Y-%m-%d %H:%M:%S")
+
+
+def main(args):
+    """Main entry point allowing external calls
+
+    Args:
+      args ([str]): command line parameter list
+    """
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    _logger.info("Starting application...")
+    print("Hello, world!")
+
+
+def run():
+    """Entry point for console_scripts
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    run()
--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup
+import setuptools
 
 
 def setup_package():
-    setup(
-        name="face_shazam",
-        version="1.0.0",
-        install_requires=["numpy>=1.13.1, < 2"],
-        setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'],
-        use_pyscaffold=True,
-
-    )
+    setuptools.setup(setup_requires=['pbr'], pbr=True)
 
 
 if __name__ == "__main__":
